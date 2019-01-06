0e967c168f697ab343119c86f1aff953cd8737ea
Carmen Renee <carmengenpt@gmail.com>
2019-01-06 21:20:40 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,11 @@
 def unsafe?(speed)
-  if speed > 60
-  "true"
-elsif speed < 40
-"true"
-else
-"false"
+  if speed > 60 
+    "true"
+    elsif speed < 40
+    "true"
+  else
+    "false"
+  
 end
 end
 
@@ -12,7 +13,5 @@ def not_safe?(speed)
 	
 end
 	
-
-
 
 
